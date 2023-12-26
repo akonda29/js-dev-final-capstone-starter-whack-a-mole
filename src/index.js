0dbed8dc32d5b04c81bb6d284835ec7889f75eac
akonda29 <64426886+akonda29@users.noreply.github.com>
2023-12-25 19:16:32 -0700

    Update index.js
    added code for clearScore, updateTimer and startTimer code

--- src/index.js
+++ src/index.js
@@ -180,7 +180,11 @@ function toggleVisibility(hole){
 */
 function updateScore() {
   // TODO: Write your code here
-
+  // Increment the points global variable by 1 point
+  points++
+   // Update score.textContent with points.
+  score.textContent = points;
+  // Return points;
   return points;
 }
 
@@ -193,8 +197,11 @@ function updateScore() {
 */
 function clearScore() {
   // TODO: Write your code here
-  // points = 0;
-  // score.textContent = points;
+  // set the points global variable to 0
+  points = 0;
+  // update score.textContent 
+  score.textContent = points;
+  // return points;
   return points;
 }
 
@@ -206,7 +213,10 @@ function clearScore() {
 function updateTimer() {
   // TODO: Write your code here.
   // hint: this code is provided to you in the instructions.
-  
+    if (time > 0){
+    time -= 1;
+    timerDisplay.textContent = time;
+  }
   return time;
 }
 
@@ -219,6 +229,7 @@ function updateTimer() {
 function startTimer() {
   // TODO: Write your code here
   // timer = setInterval(updateTimer, 1000);
+    timer = setInterval(updateTimer, 1000);
   return timer;
 }
 
