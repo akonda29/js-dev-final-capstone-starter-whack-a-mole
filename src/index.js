72eb5c9d388f13e732e5e16a02806bd986cc67b3
akonda29 <64426886+akonda29@users.noreply.github.com>
2023-12-25 18:44:15 -0700

    Update index.js
    Added code for showUp fucntion

--- src/index.js
+++ src/index.js
@@ -132,8 +132,8 @@ return "game stopped";
 *
 */
 function showUp() {
-  let delay = 0; // TODO: Update so that it uses setDelay()
-  const hole = 0;  // TODO: Update so that it use chooseHole()
+  let delay = setDelay(); // TODO: Update so that it uses setDelay()
+  const hole = chooseHole();  // TODO: Update so that it use chooseHole()
   return showAndHide(hole, delay);
 }
 
